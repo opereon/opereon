7613194a35b60f7649024406c2f233ee16b57ef7
Maksymilian Ciak <maksymilian.ciak@kodegenix.pl>
2019-09-10 11:22:45 +0200

    Loading file.

--- rust-doc-parser/src/main.rs
+++ rust-doc-parser/src/main.rs
@@ -1,6 +1,18 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+
+    let file_to_load_path = &args[1];
+    let file_to_save_path = &args[2];
+
+    println!("File to load: {}", file_to_load_path);
+    println!("File to save: {}", file_to_save_path);
+
+    let content = fs::read_to_string(file_to_load_path)
+        .expect("Something went wrong reading the file");
+
+    println!("Content: {}", content);
 }
