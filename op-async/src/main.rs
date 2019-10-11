c8b3352a71ed59f4be013c1efbe87cc26d72ba0d
jchlapinski <jakub.chlapinski@kodegenix.pl>
2019-10-11 17:31:37 +0200

    dev

--- op-async/src/main.rs
+++ op-async/src/main.rs
@@ -12,10 +12,23 @@ use std::task::{Context, Poll, Waker};
 use std::pin::Pin;
 use std::process::Termination;
 use std::ops::Deref;
+use tokio::timer::Interval;
+use std::time::Duration;
 
 struct Operation {
     id: Uuid,
     name: String,
+    progress: f64,
+}
+
+impl Operation {
+    fn new(name: &str) -> OperationRef {
+        OperationRef::new(Operation {
+            id: Uuid::new_v4(),
+            name: name.into(),
+            progress: 0.0,
+        })
+    }
 }
 
 type OperationRef = SyncRef<Operation>;
@@ -94,7 +107,51 @@ impl Future for EngineMainTask {
             e.waker = Some(cx.waker().clone());
         }
 
+        if !self.engine.read().operation_queue1.is_empty() {
+            let mut e = self.engine.write();
+            while let Some(op) = e.operation_queue1.pop_front() {
+                let task = OperationTask::new(self.engine.clone(), op);
+                tokio::spawn(task);
+            }
+        }
+
         Poll::Ready(EngineResult { code: 0 })
+    }
+}
+
+struct OperationTask {
+    engine: EngineRef,
+    operation: OperationRef,
+    waker: Option<Waker>,
+    interval: Interval,
+}
+
+impl OperationTask {
+    fn new(engine: EngineRef, operation: OperationRef) -> OperationTask {
+        OperationTask {
+            engine,
+            operation,
+            waker: None,
+            interval: Interval::new_interval(Duration::from_secs(3)),
+        }
+    }
+}
+
+impl Future for OperationTask {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.waker.is_none() {
+            self.waker = Some(cx.waker().clone());
+        }
+
+        match self.interval.poll_next_unpin(cx) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(_) => {
+                self.engine.write().operations.remove(&self.operation.read().id);
+                Poll::Ready(())
+            }
+        }
     }
 }
 
@@ -106,10 +163,7 @@ fn main() -> EngineResult {
         builder.name_prefix("op-").build().unwrap()
     };
 
-    engine.write().enqueue_op(OperationRef::new(Operation {
-        id: Uuid::nil(),
-        name: String::from("ddd"),
-    }));
+    engine.write().enqueue_op(Operation::new("ddd"));
 
     runtime.block_on(async move {
         engine.main_task().await
