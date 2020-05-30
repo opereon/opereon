b9535eaf15c42c9ad9fd33bb347813b183db6001
jchlapinski <jakub.chlapinski@kodegenix.pl>
2020-05-30 18:54:00 +0200

    dev

--- op-exec2/src/command/mod.rs
+++ op-exec2/src/command/mod.rs
@@ -25,7 +25,7 @@ async fn execute(mut command: Command, log: &OutputLog) -> Result<(), std::io::E
     async fn stdout_read<R: AsyncRead + Unpin>(s: BufReader<R>) -> Result<(), std::io::Error> {
         let mut stdout = s.lines();
         while let Some(line) = stdout.next_line().await? {
-            println!("out: {}", line);
+            println!("out: {:?}", line);
         }
         println!("out: ---");
         Ok(())
@@ -34,7 +34,7 @@ async fn execute(mut command: Command, log: &OutputLog) -> Result<(), std::io::E
     async fn stderr_read<R: AsyncRead + Unpin>(s: BufReader<R>) -> Result<(), std::io::Error> {
         let mut stderr = s.lines();
         while let Some(line) = stderr.next_line().await? {
-            println!("err: {}", line);
+            println!("err: {:?}", line);
         }
         println!("err: ---");
         Ok(())
@@ -45,6 +45,14 @@ async fn execute(mut command: Command, log: &OutputLog) -> Result<(), std::io::E
     status(child).await
 }
 
+async fn execute_pty(command: std::process::Command, log: &OutputLog) -> Result<(), std::io::Error> {
+    let mut session = spawn_command(command, None).expect("spawn");
+    while let Ok(line) = session.read_line() {
+        println!("out: {:?}", line);
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -53,6 +61,25 @@ mod tests {
     fn ssh_command() {
         let mut cmd = Command::new("/usr/bin/bash");
         cmd.arg("-c").arg("for i in {1..10}; do echo stdout output; echo stderr output 1>&2; sleep 0.1; done;");
+
+        let log = OutputLog::new();
+
+        let mut rt = tokio::runtime::Runtime::new().expect("runtime");
+
+        rt.block_on(async move {
+            execute(cmd, &log).await.expect("error");
+        });
+    }
+
+    #[test]
+    fn aaa_command() {
+        let mut cmd = tokio::process::Command::new("/usr/bin/rsync");
+        cmd.arg("-av")
+            .arg("--stats")
+            .arg("--progress")
+            .arg("--bwlimit=200k")
+            .arg("./../target/debug/incremental")
+            .arg("./../target/debug2");
 
         let log = OutputLog::new();
 
